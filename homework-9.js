feb808e0ce4060406dfa98719d5fb85e3a9f19fd
KaiouKaku <796311@mail.ru>
2026-08-05 20:44:01 +0700

    fixed errors

--- homework-9.js
+++ homework-9.js
@@ -4,10 +4,12 @@ import { userComments } from "./comments.js";
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const newArray = numbers.filter(num => num >= 5);
 console.log(newArray);
+
 //№3 Создал массив строк и через метод includes() проверил наличие определенной строки в массиве.
 const kitchenUtensils = [ "fork", "knife", "spoon", "mug", "plate", "cup" ];
 const findDevice = kitchenUtensils.includes("mug");
 console.log(findDevice);
+
 //№4 Создал функцию, которая принимает массив и возвращает его в обратном порядке.
 function reverseArray(array) {
     return array.reverse();
@@ -16,30 +18,27 @@ const reverseNumbers = reverseArray(numbers);
 const reverseKitchenUtensils = reverseArray(kitchenUtensils);
 console.log(reverseNumbers);
 console.log(reverseKitchenUtensils);
+
 //№7 Создал новый массив из объектов, в котором будут только те объекты, у которых email содержит ".com".
 const findComment = userComments.filter(comment => comment.email.includes(".com"));
 console.log(findComment);
+
 //№8 Создал новый массив из объектов, в котором первые 5 объектов будут иметь postId = 2, а остальные postId = 1.
-const updatedComments = userComments.map(comment => {
-    if (comment.id <= 5) {
-        return { ...comment, postId: 2 };
-    }   else {
-        return { ...comment, postId: 1 };
-    }
-});
+const updatedComments = userComments.map(comment =>
+    comment.id <= 5 ? { ...comment, postId: 2 } : { ...comment, postId: 1 }
+);
 console.log(updatedComments);
+
 //№9 Создал новый массив из объектов, в котором будут только id и name.
 const shortUserComment = userComments.map ( comment => ({ id: comment.id, name: comment.name }));
 console.log(shortUserComment);
+
 //№10 Создал новый массив из объектов, в котором будет новое свойство isInvalid, которое будет true, если длина body больше 180 символов, и false в противном случае.
-const sortingUserComments = userComments.map(comment => {
-    if (comment.body.length > 180) {
-        return { ...comment, isInvalid: true };
-    }   else {
-        return { ...comment, isInvalid: false };
-    }
-});
+const sortingUserComments = userComments.map(comment =>
+    comment.body.length > 180 ? { ...comment, isInvalid: true } : { ...comment, isInvalid: false }
+);
 console.log(sortingUserComments);
+
 //№11 Перебрал массив с помощью метода reduce() и map(), в котором будет только email каждого комментария.
 const mailArrayReduce = userComments.reduce((acc, comment) => {
     return [...acc, comment.email];
@@ -48,6 +47,7 @@ console.log(mailArrayReduce);
 
 const mailArrayMap = userComments.map(comment => comment.email);
 console.log(mailArrayMap);
+
 //№12 Привел к строке массив с задания 11.
 const mailArrayString = mailArrayMap.join(", ");
 console.log(mailArrayString);
